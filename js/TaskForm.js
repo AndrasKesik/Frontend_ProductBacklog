8cf11838e43a295c93f81a1253522249aeda2068
Daniel Koics <koics.dani@gmail.com>
2016-05-25 19:23:08 +0200

    Make an experience on setting up indexed-DB
    * Code not working, but may be useful for future developments
      -- problems with events when loading
    * button type changed from 'submit' to 'button'
      since it does not reload page
    * changes only in the separated file

--- js/TaskForm.js
+++ js/TaskForm.js
@@ -2,10 +2,53 @@
  * Created by KoicsD on 2016.05.24..
  */
 (function (app) {
+    app.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+
+    app.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+    app.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+
+    app.db_name = 'backlog_items';
+    app.db_version = 1;
+
+    if (!app.indexedDB) {
+        alert("Your browser doesn't support a stable version of IndexedDB.");
+    } else {
+        (function openDB() {
+            console.log('opening DB...');
+            var req = indexedDB.open(app.db_name, app.db_version);
+            req.onsuccess = function (event) {
+                console.log('DB opened successfully.');
+                app.db = req.result;
+            };
+            req.onerror = function (event) {
+                console.log('Error while opening database. Error code: ' + event.target.errorCode);
+                alert(':( Sorry, an error has occurred while opening database. Error code: ' + event.target.errorCode);
+            };
+        })();
+    }
+
     (function (subapp) {
+        subapp.store_name = 'blabla';
         subapp.validateTitle = validateTitle;
         subapp.validateDescription = validateDescription;
+        subapp.saveTask = saveTask;
+        subapp.discardForm = discardForm;
 
+        // initializing indexed-DB:
+        (function () {
+            if (!!app.db && !app.db.objectStoreNames.contains(subapp.store_name)) {
+                try {
+                    console.log('Creating object-store...');
+                    app.db.createObjecStore(subapp.store_name);
+                    console.log('Object-store created successfully.')
+                } catch (error) {
+                    console.log('Error while creating object-store:\n' + error.toString());
+                    alert(':( Sorry, an error occurred while creating object-store. See console.');
+                }
+            }
+        })();
+
+        // validation-logic:
         var validity = {
             title: false,
             description: false
@@ -48,6 +91,47 @@
         function refreshSavable() {
             var button = document.getElementById('btnSaveTask');
             button.disabled = !(validity.title && validity.description);
+        }
+
+        // save and cancel:
+        function saveTask() {
+            var title = document.getElementById('inpTitle').value;
+            var description = document.getElementById('txtDesc').value;
+            if (!app.db.objectStoreNames.contains(subapp.store_name)) {
+                console.log('Object-store cannot be found in indexedDB when saving Task.');
+                alert(':( Sorry, object-store cannot be found!');
+            } else {
+                try {
+                    console.log('Saving Task from form...');
+                    var transaction = app.db.transaction(subapp.store_name, 'readwrite');
+                    var objectStore = transaction.objectStore(subapp.store_name);
+                    var request = objectStore.add({title: title, description: description});
+                    request.onsuccess = function (event) {
+                        console.log('Task saved from form successfully.');
+                        alert(';) Task has been saved successfully:\n'
+                            + '{\n'
+                            + '\ttitle: ' + title + '\n'
+                            + '\tdescription: ' + description + '\n'
+                            + '}');
+                    };
+                    request.onerror = function (event) {
+                        console.log('Error when saving Task from form. Error code: ' + event.target.errorCode);
+                        alert(':( An error has occurred when saving Task from form. Error code: '
+                            + event.target.errorCode);
+                    }
+                } catch (error) {
+                    console.log('Error while saving Task from form:\n' + error.toString());
+                    alert(':( Sorry, an error occurred while creating object-store. See console.');
+                }
+            }
+        }
+
+        function discardForm() {
+            document.getElementById('inpTitle').value = '';
+            document.getElementById('txtDesc').value = '';
+            validity.title = false;
+            validity.description = false;
+            refreshSavable();
         }
     })(app.task_form = app.task_form || {});
 })(window.product_backlog = window.product_backlog || {});
